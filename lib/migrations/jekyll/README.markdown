f141ec557b1bfe866148ea9855d795ec966b78d7
Jared Morgan <jmorgan@morgancreative.net>
2009-09-26 20:04:12 -0500

    Work on migration script from Wistle to Jekyll - moving comments to Disqus

---
+++ lib/migrations/jekyll/README.markdown
@@ -0,0 +1,5 @@
+#Migration to Jekyll
+
+The scripts in this folder are intended to assist in migrating from 
+[Wistle](http://github.com/jm81/wistle) to
+[Jekyll](http://github.com/mojombo/jekyll).
---
+++ lib/migrations/jekyll/disqus_comments.rb
@@ -0,0 +1,67 @@
+require 'builder'
+require 'ipaddr'
+
+module JekyllMigration
+  # Disqus, naturally, doesn't have a generic import option (so far as I can
+  # tell. It does import from the xml exported by intensedebate.com . By exporting
+  # from IntenseDebate, it's possible to determine an xml format that will be
+  # accepted by Disqus.
+  #
+  # See intense_debate_sample.xml for an example.
+  #
+  # merb -i production
+  # > File.open(filename, 'w') do |f|
+  # >   f.write JekyllMigration::DisqusComments.new(site_id, prefix).to_xml(f)
+  # > end
+  #
+  # In Disqus admin for your site, go to Tools > Import/Export
+  # Choose "IntenseDebate" and upload the generate xml file.
+  #
+  # **This does not take into account replies to comments.**
+  class DisqusComments
+    # ==== Parameters
+    # site_id<Integer>:: ID of Site
+    # prefix<String>:: prefix for paths, e.g. "http://example.com/"
+    def initialize(site_id, prefix)
+      @site = Site.get(site_id)
+      @prefix = prefix
+    end
+  
+    def to_xml(file)
+      xml = Builder::XmlMarkup.new(:target => file)
+      xml.instruct!
+      xml.output do
+        @site.articles.each do |a|
+          if a.comments.count > 0
+            xml.blogpost do
+              # Article information
+              xml.url "#{@prefix}#{a.path}"
+              xml.title a.title
+              xml.guid "#{@prefix}#{a.path}"
+              
+              # Comments
+              xml.comments do
+                a.comments.each do |c|
+                  xml.comment do
+                    xml.isAnon 0
+                    xml.name c.author
+                    xml.email c.email
+                    # Note that self-closing tags cause import to fail.
+                    xml.url '' # Not used by Wistle
+                    xml.ip '' # Not used by Wistle, but would need to be a decimal representation
+                    xml.comment c.body
+                    xml.date c.created_at.strftime('%Y-%m-%d %H:%M:%S')
+                    xml.gmt c.created_at.to_time.utc.strftime('%Y-%m-%d %H:%M:%S')
+                    xml.score 1 # Just go with default
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+      xml.target!
+    end # to_xml
+   
+  end
+end
